344632f73d1add47e35478083b326d3f62bb38cf
Benoit Quette <benoit.quette@gmail.com>
2024-04-07 16:16:12 +0200

    simplify code

--- src/hooks/use-cart.js
+++ src/hooks/use-cart.js
@@ -5,16 +5,12 @@ import useLocalStorageState from './use-local-storage-state';
 export default function useCart() {
   const { current, setItemValue } = useLocalStorageState('cart', []);
 
-  const updateBooksInCart = (newBooks) => {
-    setItemValue(newBooks);
-  };
-
   const addBook = (id, name, coverUrl, author) => {
     if (!current.reduce((found, book) => (book.id === id ? true : found), false))
-      updateBooksInCart(current.concat([{ id, coverUrl, name, author }]));
+      setItemValue(current.concat([{ id, coverUrl, name, author }]));
   };
   const removeBook = (id) => {
-    updateBooksInCart(current.filter((book) => book.id !== id));
+    setItemValue(current.filter((book) => book.id !== id));
   };
 
   return { books: current, addBook, removeBook };
