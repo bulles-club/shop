b0763ae88d27384a4a1a663688a50e6402ef4562
Benoit Quette <benoit.quette@gmail.com>
2024-04-20 09:46:18 +0200

    downgrade eslint to v8

--- eslintrc.config.js
+++ eslintrc.config.js
@@ -6,7 +6,7 @@ export default [
       es2021: true,
     },
     plugins: ['perfectionist', 'unused-imports', 'prettier'],
-    extends: ['prettier', 'plugin:@next/next/recommended'],
+    extends: ['airbnb', 'airbnb/hooks', 'prettier', 'plugin:@next/next/recommended'],
     parserOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
